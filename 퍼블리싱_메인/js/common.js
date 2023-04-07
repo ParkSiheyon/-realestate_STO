66aa2a197465b3c88e97d1da9aa318cbfe9cf7fd
shkim00216 <shkim00216@gmail.com>
2023-04-07 14:07:15 +0900

    ㅇㅇ

--- 퍼블리싱_메인/js/common.js
+++ 퍼블리싱_메인/js/common.js
@@ -21,34 +21,4 @@ $(function(){
       $(this).addClass('on');
     });
 
-    /* 메뉴바 활성화 */
-
-    $(".nav li").click(function(){
-      $(".nav li").removeClass('on');
-      $(this).addClass('on');
-    });
-
-    /* 탭메뉴 활성화 */
-
-    $(".top_tab li").click(function(){
-      $(".top_tab li").removeClass('on');
-      $(this).addClass('on');
-    });
-
-    $(".small_tab li").click(function(){
-      $(".small_tab li").removeClass('on');
-      $(this).addClass('on');
-    });
-
-    /* 탭 누르면 내용 보이게 */
-
-    for (let i=0; i<$('.small_tab li').length; i++){
-      $('.small_tab li').eq(i).on('click', function(){
-        $(".small_tab li").removeClass('on');
-        $(".cont_area .cont").removeClass('on');
-        $(".small_tab li").eq(i).addClass('on');
-        $(".cont_area .cont").eq(i).addClass('on');
-      })
-    }
-
 });
